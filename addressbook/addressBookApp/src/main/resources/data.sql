49c41ed5ce9058a7e2cbb0082177351beb4dbb27
Nihad ELBARI <e2405911@ensimlan.univ-lemans.fr>
2025-01-08 18:20:15 +0100

    projet

---
+++ addressbook/addressBookApp/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO address (id,content, author) VALUES (1,'123 Rue de Paris', 'Jean Dupont');
+INSERT INTO address (id,content, author) VALUES (2,'456 Avenue des Champs-Élysées', 'Marie Martin');
+
+
+COMMIT;
