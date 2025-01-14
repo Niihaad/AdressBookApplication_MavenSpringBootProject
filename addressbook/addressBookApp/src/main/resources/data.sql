2d9bdb868191852bd73eb3551f6aeb1dd94d3d55
Nihad El Bari <nihadelbari7@gmail.com>
2025-01-14 02:05:33 +0100

    Delete addressbook directory

--- addressbook/addressBookApp/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-
-INSERT INTO address (id,content, author) VALUES (1,'123 Rue de Paris', 'Jean Dupont');
-INSERT INTO address (id,content, author) VALUES (2,'456 Avenue des Champs-Élysées', 'Marie Martin');
-
-
-COMMIT;
